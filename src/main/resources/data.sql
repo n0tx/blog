b07be3f3f2ef7900328660db17f3502e98082257
n0tx <rcandra91@msn.com>
2023-11-20 13:04:23 +0700

    add initial data to table - add data.sql for auto insert to posts and users tables when application startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT
+INTO
+  users
+  (name, username, email, password)
+VALUES
+  ('Riki Candra', 'riki', 'riki@mail.com', '12345');
+
+INSERT
+INTO
+  posts
+  (title, body, user_id)
+VALUES
+  ('title post 1', 'body post 1', 1);
+
+INSERT
+INTO
+  posts
+  (title, body, user_id)
+VALUES
+  ('title post 2', 'body post 2', 1);
+
+INSERT
+INTO
+  posts
+  (title, body, user_id)
+VALUES
+  ('title post 3', 'body post 3', 1);
